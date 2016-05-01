035d48d4c384aeeabb01403bccd4d5f922294407
mavocado3 <gustavopetruzzi19@gmail.com>
2016-05-01 01:57:55 -0300

    Added files via upload

--- modeloParcialC1/main.c
+++ modeloParcialC1/main.c
@@ -5,7 +5,6 @@
 
 int main()
 {
-    int i;
     eProductos misproductos[50];
     int auxInt;
     char opcion;
@@ -15,6 +14,9 @@ int main()
     int cantidad;
     char descripcion[50];
     int isEmpty = 1;
+    float promedio;
+    float total;
+    int mayoresPromedio;
 
     auxInt = initArray(misproductos, MAX);
     if(auxInt== -1)
@@ -34,7 +36,7 @@ int main()
                 {
                     pedirDatos(&codigoProducto, &importe, &cantidad, descripcion,1);
                     misproductos[indice] = cargarProducto(codigoProducto, importe, cantidad, descripcion);
-                    printf("producto cargado!");
+                    printf("producto cargado!\n");
                     isEmpty = 0;
                 }
                 else
@@ -47,25 +49,52 @@ int main()
             case '2':
                 if(isEmpty == 0)
                 {
-                    auxInt = getInt( &codigoProducto,"Ingrese el codigo a buscar", "ERROR: solo se permiten numeros del 1 al 1000", 1, 1000);
+                    auxInt = getInt( &codigoProducto,"Ingrese el codigo a modificar: ", "ERROR: solo se permiten numeros del 1 al 1000", 1, 1000);
+                    system("cls");
                     if(auxInt == 0)
                     {
                         indice = buscarPorCodigo(misproductos, MAX, codigoProducto);
+                        if(indice!= -1)
+                        {
+                            pedirDatos(&codigoProducto, &importe, &cantidad, descripcion, 0);
+                            misproductos[indice] = cargarProducto(codigoProducto, importe, cantidad, descripcion);
+                        }
+                        else
+                        {
+                            printf("No existe ningun producto con ese codigo\n!");
+                        }
                     }
                 }
                 break;
             case '3':
-                break;
-            case '4':
-                for(i=0;i<MAX;i++)
+                if(isEmpty ==0)
                 {
-                    if(misproductos[i].isEmpty == 0)
+                    auxInt = getInt(&codigoProducto, "Ingrese el codigo a borrar:", "ERROR: solo se permiten numeros del 1 al 1000", 1, 1000);
+                    if(auxInt== 0)
                     {
-                        printf("Codigo: %d\nImporte:%.2f\nCantidad:%d\nDescripcion:%s", misproductos[i].codigoProducto, misproductos[i].importe, misproductos[i].cantidad, misproductos[i].descripcion);
+                        indice = buscarPorCodigo(misproductos, MAX, codigoProducto);
+                        if(indice != -1)
+                        {
+                            misproductos[indice].isEmpty = 1;
+                        }
+                        else
+                        {
+                            printf("No existe ningun producto con ese codigo\n!");
+                        }
                     }
                 }
                 break;
+            case '4':
+                indice = buscarMayorMenor(misproductos, MAX, 1);
+                printf("Datos:\nCodigo: %d\nDescripcion: %s\nImporte: %.2f\nCantidad: %d\n", misproductos[indice].codigoProducto, misproductos[indice].descripcion, misproductos[indice].importe, misproductos[indice].cantidad);
+                calculosImportes(misproductos, &total, &promedio, &mayoresPromedio, MAX);
+                printf("Importe total: %.2f\n", total);
+                printf("Promedio: %.2f\n", promedio);
+                printf("Mayores promedio: %d\n", mayoresPromedio);
+                break;
             case '5':
+                ordenaPorCantidad(misproductos, MAX);
+                mostrarProductos(misproductos, MAX);
                 break;
             case '6':
                 break;
